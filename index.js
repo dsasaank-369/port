f73efbc5a2505eb6f84078318aee928e8cffbb7d
Desu Tara Sasaank <80057285+dsasaank-369@users.noreply.github.com>
2022-01-21 19:05:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+  strings: ["Student", "Web Developer", "Java Aspirant"],
+  typeSpeed: 110,
+  backSpeed: 60,
+  loop: true,
+});
+
+// const hamburger = document.getElementById("hamburger");
+// const navUL = document.getElementById("nav-ul");
+
+// hamburger.addEventListener("click", () => {
+//   navUL.classList.toggle("show");
+// });
